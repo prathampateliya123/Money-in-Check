f3e42054e27419b149afa7c453f76b66ef2843bb
prathampateliya123 <136707851+prathampateliya123@users.noreply.github.com>
2026-08-11 23:37:38 +0530

    launguages in background change

--- next.config.js
+++ next.config.js
@@ -3,6 +3,19 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async rewrites() {
+    return [
+      {
+        source: '/:lang(es|fr|de|hi)',
+        destination: '/',
+      },
+      {
+        source: '/:lang(es|fr|de|hi)/:path*',
+        destination: '/',
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
+
